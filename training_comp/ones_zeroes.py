2351af593da9725bc04349ed2eeb1229c64c877e
Mohamed Naas <mohammadnaas203@gmail.com>
2024-09-18 11:19:06 +0200

    placed the correct answer into the question

--- training_comp/ones_zeroes.py
+++ training_comp/ones_zeroes.py
@@ -1,11 +1,15 @@
 n = int(input())
 ones_zeroes = input()
-i = 0
 
-while i < len(ones_zeroes)-1:
-    if ones_zeroes[i:i+1].is '010':
-        ones_zeroes = ones_zeroes[:i-1]+ones_zeroes[i+2:]
-        i -= 1
-        continue
-    i += 1
-print(len(ones_zeroes))
+# my wrong answer
+# i = 0
+
+# # while i < len(ones_zeroes)-1:
+# #     if ones_zeroes[i:i+1].is '010':
+# #         ones_zeroes = ones_zeroes[:i-1]+ones_zeroes[i+2:]
+# #         i -= 1
+# #         continue
+# #     i += 1
+# # print(len(ones_zeroes))
+
+print(abs(ones_zeroes.count('0')-ones_zeroes.count('1')))
